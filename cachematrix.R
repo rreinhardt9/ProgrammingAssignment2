104785789b8b73381a8d251637bcd565a30dfaa9
Ross Reinhardt <rreinhardt9@gmail.com>
2015-04-21 23:00:44 -0400

    Create special cached matrix and function to find inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCachedMatrix create a special matrix list object with getters and setters for it's inverse
+## cahceSolve uses the cached inverse if available, otherwise it calculates and sets the inverse
 
-## Write a short comment describing this function
+## Create special cached matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) s <<- solve
+  getSolve <- function() s
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## Find inverse of matrix using special cached matrix above
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  s <- x$getSolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  x$setSolve(s)
+  s
 }
+
+## For testing
+testMatrix <- matrix(runif(100,1,80), 10, 10)
